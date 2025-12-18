731496d11add1bcabce5ab67c81a4e653d3450c8
veresandras1 <veres.andras1@gmail.com>
2025-12-18 13:49:12 +0100

    Update tasks.py

--- src/crew/tasks.py
+++ src/crew/tasks.py
@@ -1,1 +1,9 @@
-﻿
+from crewai import Task
+
+class IDSCrewTasks:
+    def cleaning_task(self, agent, input_file, output_file):
+        return Task(
+            description=f"Elemezd a {input_file} fájlt, távolítsd el a zajt és mentsd el a tisztított mintát ide: {output_file}.",
+            expected_output="Statisztikai jelentés a tisztítás sikerességéről.",
+            agent=agent
+        )﻿
